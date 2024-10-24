ff7d63d155874de29d1fece7af8308ba753704f3
Mehdi Ghozelam <mehdighozelam@MacBook-Air-de-Mehdi.local>
2024-10-24 15:05:24 +0200

    correction

--- portfolio/src/components/Presentation.jsx
+++ portfolio/src/components/Presentation.jsx
@@ -10,7 +10,7 @@ const Presentation = () => {
         </div>
         <div className="about-info">
           <h2 id="about-title">À propos de moi</h2>
-          <p>Je m'appelle Mehdi, et je suis une développeur web passionné par la création de solutions numériques modernes et innovantes. J'ai suivi une formation complète en développement web avec OpenClassrooms, où j'ai perfectionné mes compétences dans la conception de sites performants et responsives, ainsi que dans l'utilisation de frameworks modernes comme React. Je suis constamment à la recherche de nouveaux défis pour améliorer mes compétences et répondre aux besoins de mes clients.</p>
+          <p>Je m'appelle Mehdi, et je suis un développeur web passionné par la création de solutions numériques modernes et innovantes. J'ai suivi une formation complète en développement web avec OpenClassrooms, où j'ai perfectionné mes compétences dans la conception de sites performants et responsives, ainsi que dans l'utilisation de frameworks modernes comme React. Je suis constamment à la recherche de nouveaux défis pour améliorer mes compétences et répondre aux besoins de mes clients.</p>
           <p>Mon objectif est de concevoir des sites web performants, esthétiques et optimisés pour offrir une expérience utilisateur exceptionnelle. Explorez mon portfolio pour en savoir plus sur mes projets et mon parcours.</p>
         </div>
       </div>
